9198a1700232efa2458f3b980d255bc4b362a982
Abdullah Javed <abdullahjaved@Abdullahs-MacBook-Air.local>
2026-06-24 09:23:43 +0200

    feat: add unique student IDs and enhance record management

--- Students_Record_Management_v1.py
+++ Students_Record_Management_v1.py
@@ -2,16 +2,17 @@ students = []
 
 
 def add_student(students):
-    num = int(input("(How many students record would you like to add?): "))
+    num = get_integer("(How many students record would you like to add?): ")
     for i in range(num):
         name = input("Enter Name: ")
-        age = int(input("Enter age: "))
-        grades = float(input("Enter Grades: "))
+        age = get_integer("Enter age: ")
+        grades = get_grades("Enter Grades: ")
 
         student = {
+            "id": get_nxt_ID(students),
             "name": name,
             "age": age,
-            "grades": grades
+            "grades": grades,
         }
 
         students.append(student)
@@ -24,29 +25,79 @@ def view_students(students):
         print("Student Record is Empty.")
         return
 
-    count = 1
-    print("  | Name    | Age | Grades |")
+    print(" ID | Name    | Age | Grades ")
     for student in students:
         print(
-            f"{count}. {student['name']} - {student['age']} - {student['grades']}")
-        count += 1
+            f"{student['id']} - {student['name']} - {student['age']} - {student['grades']}"
+        )
 
 
 def search_student(students):
     if not students:
         print("Student Record is Empty.")
         return
-    s_name = input("Enter Student Name: ")
+    s_id = get_integer("Enter Student ID: ")
     found = False
-    for s in students:
-        if s["name"] == s_name:
-            print("STUDENT RECORD:")
-            print(f" {s['name']} - {s['age']} - {s['grades']}")
+    for student in students:
+        if student["id"] == s_id:
+            print("Student Record:")
+            print(
+                f"{student['id']} - {student['name']} - {student['age']} - {student['grades']}"
+            )
             found = True
             break
+    # for s in students:             //Later will Improve this feature by adding option for searching by name and ID
+    #     if s["name"].lower() == s_name.lower():
+    #         print("STUDENT RECORD:")
+    #         print(f" {s['name']} - {s['age']} - {s['grades']}")
+    #         found = True
+    #         break
 
     if not found:
         print("Student Record Not Found.")
+
+
+def delete_student(students):
+    if not students:
+        print("Student Record is Empty.")
+        return
+
+    s_id = get_integer("Enter Student ID: ")
+
+    for student in students:
+        if student["id"] == s_id:
+            students.remove(student)
+            print("Student's Record Deleted Successfully..")
+            break
+    else:
+        print("Student's record not found.")
+
+
+def update_student(students):
+    if not students:
+        print("Student Record is Empty.")
+        return
+
+    s_id = get_integer("Enter Student ID: ")
+
+    for student in students:
+        if student["id"] == s_id:
+            print("Current Record:")
+            print(
+                f"{student['id']} - {student['name']} - {student['age']} - {student['grades']}"
+            )
+            print("\nNow Update the Record: ")
+
+            new_name = input("Enter new name: ")
+            new_age = get_integer("Enter new age: ")
+            new_grades = get_float("Enter new grades: ")
+
+            student.update({"name": new_name, "age": new_age, "grades": new_grades})
+
+            print("Student's Record Updated Successfully..")
+            break
+    else:
+        print("Student's record not found.")
 
 
 def top_student(students):
@@ -60,7 +111,7 @@ def top_student(students):
 
             top = student
 
-    print(f"{top['name']} - {top['age']} - {top['grades']}")
+    print(f"{top['id']} - {top['name']} - {top['age']} - {top['grades']}")
 
 
 def average_grades(students):
@@ -79,11 +130,14 @@ def average_grades(students):
     print(f"Average grades: {avg_grades:.2f}")
 
 
+# File Handling *******************************************************************************
+
+
 def save_data(students):
     file = open("record.txt", "w")
 
     for student in students:
-        line = f"{student['name']},{student['age']},{student['grades']}"
+        line = f"{student['id']},{student['name']},{student['age']},{student['grades']}"
         file.write(line)
         file.write("\n")
 
@@ -98,9 +152,10 @@ def load_data(students):
             line = line.strip()
             parts = line.split(",")
             student = {
-                "name": parts[0],
-                "age": int(parts[1]),
-                "grades": float(parts[2])
+                "id": int(parts[0]),
+                "name": parts[1],
+                "age": int(parts[2]),
+                "grades": float(parts[3]),
             }
             students.append(student)
         print("File loaded successfully :)")
@@ -109,19 +164,61 @@ def load_data(students):
         print("No saved records found. Starting with an empty database.")
 
 
-load_data(students)
+# Helper Functions ****************************************************************************
 
 
-while True:
+def get_integer(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid Input :( -> Please enter a number.")
+
+
+def get_float(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid Input :( -> Please enter a number.")
+
+
+def get_grades(prompt):
+    while True:
+        value = get_float(prompt)
+        if value >= 0 and value <= 100:
+            return value
+        print("Grades must be between 0 and 100.")
+
+
+def get_nxt_ID(students):
+
+    max_id = 0
+    for student in students:
+        if student["id"] > max_id:
+            max_id = student["id"]
+
+    return max_id + 1
+
+
+load_data(students)  #  Automatic Data Loading
+
+
+while True:  #  Main Menu
+
     print("--- Student Record Management ---")
     print("1. Add Student Record.")
     print("2. View All Students Record.")
     print("3. Search for a Student record.")
     print("4. Show Top Student.")
     print("5. Show average grades.")
+    print("6. Delete Student's record.")
+    print("7. Update Student's record.")
     print("0. Exit System.")
 
-    choice = int(input("Enter Number(0 - 5): "))
+    choice = get_integer("Enter Number(0 - 7): ")
 
     match choice:
         case 1:
@@ -139,9 +236,12 @@ while True:
         case 5:
             average_grades(students)
 
+        case 6:
+            delete_student(students)
+
+        case 7:
+            update_student(students)
+
         case 0:
             save_data(students)
-            break
-
-        case _:
-            print(":(Invalid Number... Try Again!)")
+            break  #  Exit ;)
